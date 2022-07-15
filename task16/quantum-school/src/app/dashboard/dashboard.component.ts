3e9cd8812a1b585d5672efd8f2faa4c97366ca8f
motidivya <divyesh.vishwakarma@zeuslearning.com>
2022-07-16 05:24:00 +0530

    Add task 17 and 18

--- task16/quantum-school/src/app/dashboard/dashboard.component.ts
+++ task16/quantum-school/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { DataService } from '../core/data.service';
+import { SorterService } from '../core/sorter.service';
 import { ICourse } from '../shared/interfaces';
 
 @Component({
@@ -9,14 +10,44 @@ import { ICourse } from '../shared/interfaces';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  courses!: ICourse[];
-  constructor(private dataService: DataService) { }
+  private _courses: ICourse[] = [];
+  @Input() get courses(): ICourse[]{
+    return this._courses;
+  }
+  set courses(value: ICourse[]){
+    if(value){
+      this.filteredCourses = this._courses = value;
+      this.calculateCourses();
+    }
+  }
+  constructor(private dataService: DataService, private sorterService: SorterService) { }
 
+  filteredCourses: any[] = [];
+  coursesTotal!: number;
   ngOnInit() {
     this.dataService.getCourses()
     .subscribe((courses: ICourse[]) => {
-      return this.courses = courses;
+      return this.filteredCourses = courses;
     })
+  }
+
+  calculateCourses() {
+    this.coursesTotal = this.filteredCourses.length;
+  }
+
+  // filter(data: string) {
+  //   if (data) {
+  //       this.filteredCourses = this.courses.filter((cou: ICourse) => {
+  //           return cou.name.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
+  //                  cou.subject.name.toLowerCase().indexOf(data.toLowerCase()) > -1
+  //       });
+  //   } else {
+  //       this.filteredCourses = this.courses;
+  //   }
+  //   this.calculateCourses();
+  // }
+  sort(event: any) {
+    this.sorterService.sort(this.filteredCourses, event.target.value);
   }
 
 }
---
+++ task17/navbar.ts
@@ -0,0 +1,11 @@
+const dashboard_button:HTMLButtonElement = document.querySelector('.dashboard-button');
+const dashboard:HTMLDivElement = document.querySelector('.dashboard');
+const content_button:HTMLButtonElement = document.querySelector('.content-button');
+const content:HTMLDivElement = document.querySelector('.content');
+const users_button:HTMLButtonElement = document.querySelector('.users-button');
+const users:HTMLDivElement = document.querySelector('.users');
+const reports_button:HTMLButtonElement = document.querySelector('.reports-button');
+const reports:HTMLDivElement = document.querySelector('.reports');
+const admin_button:HTMLButtonElement = document.querySelector('.admin-button');
+const admin:HTMLDivElement = document.querySelector('.admin');
+
