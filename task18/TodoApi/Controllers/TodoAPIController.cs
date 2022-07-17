bc49d214e64bfee4e1692d5c86a96f6e84b4bf3e
motidivya <divyesh.vishwakarma@zeuslearning.com>
2022-07-17 13:08:13 +0530

    Update task 18 and 19

--- task18/TodoApi/Controllers/TodoAPIController.cs
+++ task18/TodoApi/Controllers/TodoAPIController.cs
@@ -1,27 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
-using TodoApi.Models;
-namespace TodoApi.Controllers;
+// using Microsoft.AspNetCore.Mvc;
+// using TodoApi.Models;
+// namespace TodoApi.Controllers;
 
 
-[ApiController]
-[Route("[controller]")]
-public class TodoAPIController : ControllerBase
-{
+// [ApiController]
+// [Route("[controller]")]
+// public class TodoAPIController : ControllerBase
+// {
 
-    private TodoContext _context;
+//     private TodoContext _context;
 
-    public TodoAPIController(TodoContext context)
-    {
-        _context = context;
-    }
+//     public TodoAPIController(TodoContext context)
+//     {
+//         _context = context;
+//     }
 
-    [HttpPost]
-    public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
-    {
-        _context.TodoItems.Add(todoItem);
-        await _context.SaveChangesAsync();
+//     [HttpPost]
+//     public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+//     {
+//         _context.TodoItems.Add(todoItem);
+//         await _context.SaveChangesAsync();
 
-        // return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-        return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
-    }
-}
+//         // return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+//         return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+//     }
+// }
