0f12765595aff23f65313f9d0f208c5edd25e0ae
motidivya <divyesh.vishwakarma@zeuslearning.com>
2022-08-03 10:15:00 +0530

    Add 90% Completed Task 22-6

--- task22/task6/src/app/app.component.ts
+++ task22/task6/src/app/app.component.ts
@@ -1,20 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDragEnter, CdkDragExit } from '@angular/cdk/drag-drop';
 import { trigger, state, style, animate, transition } from '@angular/animations';
-import { rubberBandAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    rubberBandAnimation()
+    // trigger('')
   ]
 })
-export class AppComponent implements OnInit {
-  title = 'task6';
-  constructor(){}
 
+export class AppComponent implements OnInit {
+  todo:string[] = ['A', 'B', 'C'];
+  timeToExit: boolean= false;
+  done:string[] = [];
+  doneCount:number = 0;
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  elementClicked(event:any){
+    var styledElement = event.target.style;
+    if(styledElement.color !=='green'){
+      styledElement.color = 'green';
+    }
+    else if(styledElement.color === 'green'){
+      styledElement.color = 'black';
+    }
+    if(styledElement.fontSize !== '30px'){
+      styledElement.fontSize = '30px';
+    }
+    else if(styledElement.fontSize === '30px'){
+      styledElement.fontSize = '16px';
+    }
+  }
+
+  // entered(event:CdkDragEnter<string[]>){
+  //   this.doneCount+= 1;
+  //   console.log('TEntered - here are', this.doneCount,  event.item.data, 'in the basket.')
+  //   if(this.doneCount === 3){
+  //     this.timeToExit= true;
+  //   }
+  // }
+
+  // exited(event:CdkDragExit<string[]>){
+  //   this.doneCount-= 1;
+  //   console.log('Exited - There are', this.doneCount, event.item.data, 'in the basket.')
+  //   if(this.doneCount === 3){
+  //     this.timeToExit= true;
+  //   }
+  // }
+
+  entered(event: CdkDragEnter<string[]>) {
+    console.log('Entered', event.item.data);
+   }
+   
+   exited(event: CdkDragExit<string[]>) {
+     console.log('Exited', event.item.data);
+   }
+  
   ngOnInit(): void {
-    
   }
 }
--- task22/task6/src/app/app.module.ts
+++ task22/task6/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { DragDropModule } from '@angular/cdk/drag-drop'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
